83679cc208710f14cf6eaea6151ac1b63cab4b10
vkur <vak@kruso.dk>
2020-10-30 18:22:38 +0200

    Added Authorization
    (cherry picked from commit 3101b4afe31f02ef534b17be4503fed919322661)

--- src/Cafeteria.DAL/Migrations/20201016163715_AddedTokenField.Designer.cs
+++ src/Cafeteria.DAL/Migrations/20201016163715_AddedTokenField.Designer.cs
@@ -4,14 +4,16 @@ using Cafeteria.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cafeteria.DAL.Migrations
 {
     [DbContext(typeof(CafeteriaDbContext))]
-    partial class CafeteriaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201016163715_AddedTokenField")]
+    partial class AddedTokenField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Implementation/UserService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Infrastructure.Abstraction;
+using Infrastructure.ModelsDTO;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Сafeteria.DataModels.Entities;
+using Сafeteria.Services;
+using Сafeteria.Services.Helpers;
+using BC = BCrypt.Net.BCrypt;
+
+namespace Infrastructure.Implementation
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly AppSettings _appSettings;
+        private readonly IMapper _mapper;
+        public UserService(IUnitOfWork unitOfWork, IOptions<AppSettings> appSettings, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _appSettings = appSettings.Value;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDTO> Authenticate(string email, string password)
+        {
+            var user = (await _unitOfWork.UserRepository.GetAll()).SingleOrDefault(x => x.Email == email && BC.Verify(password, x.Password));
+
+            // return null if user not found
+            if (user == null)
+                return null;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.UserType.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+            await _unitOfWork.UserRepository.Update(user);
+            await _unitOfWork.Save();
+            return _mapper.Map<User, UserDTO>(user);
+        }
+
+        public async Task<bool> Authorization(UserDTO user)
+        {
+            // hash password
+            user.Password = BC.HashPassword(user.Password);
+            var result = await _unitOfWork.UserRepository.Register(_mapper.Map<UserDTO, User>(user));
+            return result;
+        }
+    }
+}
